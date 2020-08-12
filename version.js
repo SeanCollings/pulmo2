74b060b87d0f39374ae799db5633618ab6e33ad3
Sean Collings <nightharrier@gmail.com>
2020-08-12 18:49:55 +0200

    fix: updated token name

--- version.js
+++ version.js
@@ -1,7 +1,7 @@
 const BUILD_VERSION = '1.0.0';
 const ANDROID_VERSION = 2;
 const IOS_VERSION = '1.0.0';
-const RELEASE_ID = '#{RELEASE_ID}#';
+const RELEASE_ID = '#{Release.ReleaseId}#';
 
 module.exports = {
   BUILD_VERSION,
