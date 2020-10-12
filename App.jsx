79871acfdc1f5d4e74bdf7e08d8cf6c2247f98f8
Sean Collings <nightharrier@gmail.com>
2020-10-12 20:39:35 +0200

    fix: added flex 1 to app

--- App.jsx
+++ App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import * as SplashScreen from 'expo-splash-screen';
 
 import AppEntry from './AppEntry';
+import { View } from 'react-native';
 
 export default () => {
   const [splashScreenEnabled, setSplashScreenEnabled] = useState(false);
@@ -12,7 +13,7 @@ export default () => {
     });
   }, []);
 
-  if (!splashScreenEnabled) return null;
+  if (!splashScreenEnabled) return <View style={{ flex: 1 }} />;
 
   return <AppEntry />;
 };
