36b2abe28b339a56351775b8271b1cc20d47d46a
Sean Collings <nightharrier@gmail.com>
2020-11-04 14:52:24 +0200

    fix: converted android version var to int

--- app.config.js
+++ app.config.js
@@ -11,7 +11,7 @@ export default ({ config }) => {
   return {
     ...config,
     version: BUILD_VERSION,
-    android: { ...config.android, versionCode: ANDROID_VERSION },
+    android: { ...config.android, versionCode: parseInt(ANDROID_VERSION) },
     ios: { ...config.ios, buildNumber: IOS_VERSION },
     extra: {
       setupData: process.env.SETUP_DATA,
